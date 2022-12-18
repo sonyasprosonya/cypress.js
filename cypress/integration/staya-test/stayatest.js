7cb457c308bff6d3a1b3b78a3f80a3b9e4b043cb
Sofia Novikova <116005879+sonyasprosonya@users.noreply.github.com>
2022-12-18 22:30:16 +0300

    убрала лишнее

--- cypress/integration/staya-test/stayatest.js
+++ cypress/integration/staya-test/stayatest.js
@@ -4,8 +4,8 @@ describe('Тестирование формы логина Staya', function () {
     it('Позитивный кейс: верные логин и пароль', function () {
         cy.visit('https://staya.dog/');
         cy.get('#stickyHeader > section.header-bottom.transition-header > div > div > a').click();
-        cy.get('#__layout > div > main > div > div > div > section > div > form > input:nth-child(1)').type('novikovasof69@mail.ru');
-        cy.get('#__layout > div > main > div > div > div > section > div > form > input:nth-child(2)').type('Gtopisdope1');
+        cy.get('#__layout > div > main > div > div > div > section > div > form > input:nth-child(1)').type('ТУТ ДОЛЖНА БЫТЬ ВЕРНАЯ ПОЧТА');
+        cy.get('#__layout > div > main > div > div > div > section > div > form > input:nth-child(2)').type('ТУТ ДОЛЖЕН БЫТЬ ВЕРНЫЙ ПАРОЛЬ');
         cy.get('#__layout > div > main > div > div > div > section > div > form > button > span').click();
 
         cy.contains('Мои заказы')
@@ -16,8 +16,8 @@ describe('Тестирование формы логина Staya', function () {
     	cy.get('#__layout > div > div > section > div > div > div > button.box__button.box__button_ok.s-button.s-button_theme-dark').click();
     	cy.visit('https://staya.dog/');
         cy.get('#stickyHeader > section.header-bottom.transition-header > div > div > a').click();
-        cy.get('#__layout > div > main > div > div > div > section > div > form > input:nth-child(1)').type('novikovasof69@mail.ru');
-        cy.get('#__layout > div > main > div > div > div > section > div > form > input:nth-child(2)').type('Gtopisdope4');
+        cy.get('#__layout > div > main > div > div > div > section > div > form > input:nth-child(1)').type('ТУТ ДОЛЖНА БЫТЬ ВЕРНАЯ ПОЧТА');
+        cy.get('#__layout > div > main > div > div > div > section > div > form > input:nth-child(2)').type('ТУТ ДОЛЖЕН БЫТЬ НЕВЕРНЫЙ ПАРОЛЬ');
         cy.get('#__layout > div > main > div > div > div > section > div > form > button > span').click();
 
         cy.contains('Невозможно войти с предоставленными учетными данными.')
